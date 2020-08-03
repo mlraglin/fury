403e8360b04a6b4efe75dec7a0faf02e0103d09a
mlraglin <43075495+mlraglin@users.noreply.github.com>
2020-08-03 18:55:51 -0400

    Adding torus visualization example

---
+++ torus_ex.py
@@ -0,0 +1,13 @@
+from fury import primitive, window, actor
+import numpy as np
+
+scene = window.Scene()
+verts = primitive.prim_torus_vertices(roundness=(1, 1))
+torus_actor = actor.surface(verts)
+print(verts)
+torus_actor.GetProperty().BackfaceCullingOff()
+dots = actor.dots(verts)
+scene.add(dots)
+scene.add(torus_actor)
+
+window.show(scene, size=(600, 600))
