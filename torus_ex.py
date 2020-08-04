38e904a044a78bd437c03aa9841bcd2462049101
mlraglin <43075495+mlraglin@users.noreply.github.com>
2020-08-04 14:48:05 -0400

    Modifying torus example

--- torus_ex.py
+++ torus_ex.py
@@ -2,9 +2,8 @@ from fury import primitive, window, actor
 import numpy as np
 
 scene = window.Scene()
-verts = primitive.prim_torus_vertices(roundness=(1, 1))
-torus_actor = actor.surface(verts)
-print(verts)
+verts, faces = primitive.prim_torus_vertices(roundness=(1, 1))
+torus_actor = actor.surface(verts, faces)
 torus_actor.GetProperty().BackfaceCullingOff()
 dots = actor.dots(verts)
 scene.add(dots)
